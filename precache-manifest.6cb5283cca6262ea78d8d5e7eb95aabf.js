8bd336a6f892edd299fb115d599ab72fe6e7d26f
fsobh <fsobh15@icloud.com>
2022-06-02 23:47:46 -0400

    Updates

---
+++ precache-manifest.6cb5283cca6262ea78d8d5e7eb95aabf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5eb2830b3162405bd6a90a9cb2da24b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a51c3b16382958593e75",
+    "url": "./static/css/main.2cb86142.chunk.css"
+  },
+  {
+    "revision": "6b9080fcec84868b8a17",
+    "url": "./static/js/2.c5bbb4b2.chunk.js"
+  },
+  {
+    "revision": "36fcd6da056cea6b6ecabae8542b28d9",
+    "url": "./static/js/2.c5bbb4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51c3b16382958593e75",
+    "url": "./static/js/main.eeae71de.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.eeae71de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec6daddee1bcbc14378",
+    "url": "./static/js/runtime-main.925dc7fb.js"
+  },
+  {
+    "revision": "ed9a1f61803f480a83c8a0744b58d76f",
+    "url": "./static/media/coinbase~VUtjtScw.ed9a1f61.png"
+  },
+  {
+    "revision": "e7b72b08654893b25ac819832056bbd3",
+    "url": "./static/media/metamaskTop~AXiPLvjM.e7b72b08.png"
+  },
+  {
+    "revision": "67b9c88b63853487c8aecf0786f775e8",
+    "url": "./static/media/wcTop~IBjXsotr.67b9c88b.png"
+  }
+]);
